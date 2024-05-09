29f66d8cb1008756bbf9d41446996f8d2eca953f
Musa Khalilov <gglmskhl@gmail.com>
2024-05-09 22:02:07 +0300

    hide the hash

--- code.py
+++ code.py
@@ -7,9 +7,11 @@ import sys
 sys.path.insert(1, './smartbot/')
 
 config = {
-    'token': 'MTIzNjA5ODk4NTM3OTIzNzkxOQ.GrX9J5.T8ykuBNTttcM5D6nnLQnD-yG2-_7Lyy-w6DX-E',
+    'token': [77, 84, 73, 122, 78, 106, 65, 53, 79, 68, 107, 52, 78, 84, 77, 51, 79, 84, 73, 122, 78, 122, 107, 120, 79, 81, 46, 71, 65, 81, 66, 102, 116, 46, 73, 120, 56, 68, 111, 86, 100, 101, 84, 95, 76, 102, 80, 120, 112, 72, 80, 55, 111, 98, 85, 104, 70, 54, 116, 103, 45, 105, 85, 77, 52, 114, 114, 69, 89, 90, 52, 119],
     'prefix': 'prefix',
 }
+
+config['token'] = ''.join([chr(x) for x in config['token']])
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
